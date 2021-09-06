7307262f8b199aa593e1d852be90510dc3141b71
Eric Miranda <ericmiranda7@gmail.com>
2021-09-06 14:47:55 +0530

    add production settings

--- server/index.js
+++ server/index.js
@@ -2,14 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const path = require('path')
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("test");
 });
+
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+})
 
 server.listen(process.env.PORT, () => {
   console.log("listening on port ", process.env.PORT);
