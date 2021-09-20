564f09c0c1f41077e0725d2229cd6fd49702ce4a
Eric Miranda <ericmiranda7@gmail.com>
2021-09-20 13:20:18 +0530

    configure mp hands with fp

--- client/src/components/game/Floor.js
+++ client/src/components/game/Floor.js
@@ -5,10 +5,19 @@ import { useState, useEffect, useRef, useContext } from 'react'
 import { Container } from 'react-bootstrap'
 import { setCallbacks } from '../../services/socketService';
 import { AuthContext } from '../../contexts/AuthContext';
+import * as mp from '@mediapipe/hands'
+import * as mpCameraUtils from '@mediapipe/camera_utils'
+import * as mpDrawingUtils from '@mediapipe/drawing_utils'
+import * as fp from 'fingerpose'
 
 const Floor = ({ game, setGame, socket }) => {
   const { userState: authState } = useContext(AuthContext)
   const [drawPile, setDrawPile] = useState([])
+  const [ctx, setCtx] = useState(null)
+  const [GE, setGE] = useState(null)
+
+  const videoRef = useRef()
+  const canvasRef = useRef()
 
   useEffect(() => {
     setCallbacks(socket, setDrawPile)
@@ -20,6 +29,56 @@ const Floor = ({ game, setGame, socket }) => {
     socket.emit('draw card', { sid: socket.id, gameId: game.id })
   }
 
+  const onResults = (results) => {
+    ctx.save();
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    ctx.drawImage(
+      results.image, 0, 0, canvasRef.current.width, canvasRef.current.height);
+    if (results.multiHandLandmarks) {
+      for (const landmarks of results.multiHandLandmarks) {
+        /*         mpDrawingUtils.drawConnectors(ctx, landmarks, mp.HAND_CONNECTIONS,
+                  { color: '#00FF00', lineWidth: 5 }); */
+        mpDrawingUtils.drawLandmarks(ctx, landmarks, { color: '#FF0000', lineWidth: 2 });
+        const estimatedGestures = GE.estimate(landmarks, 7.5);
+        console.log(estimatedGestures)
+      }
+    }
+    ctx.restore();
+  }
+
+  useEffect(() => {
+    setCtx(canvasRef.current.getContext('2d'))
+    setGE(new fp.GestureEstimator([
+      fp.Gestures.VictoryGesture,
+      fp.Gestures.ThumbsUpGesture
+    ]))
+  }, [])
+
+
+  useEffect(() => {
+    if (ctx) {
+      const hands = new mp.Hands({
+        locateFile: (file) => {
+          return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+        }
+      });
+      hands.setOptions({
+        maxNumHands: 1,
+        minDetectionConfidence: 0.8,
+        minTrackingConfidence: 0.5
+      });
+      hands.onResults(onResults);
+
+      const camera = new mpCameraUtils.Camera(videoRef.current, {
+        onFrame: async () => {
+          await hands.send({ image: videoRef.current });
+        },
+        width: 1280,
+        height: 720
+      });
+      camera.start();
+    }
+  }, [ctx])
 
 
   return (
@@ -39,6 +98,10 @@ const Floor = ({ game, setGame, socket }) => {
       </div>
       <div className="drawpile">
         <Card card={drawPile} height={'6em'} />
+      </div>
+      <div className="container">
+        <video style={{ display: 'none' }} ref={videoRef} className="input_video"></video>
+        <canvas ref={canvasRef} className="output_canvas" width="1280px" height="720px"></canvas>
       </div>
     </Container>
   )
