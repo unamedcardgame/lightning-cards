83d701f56b3933ece16afcf421af71ab0b4d1c3d
Disha-Bundela <dishabundela@gmail.com>
2021-11-10 22:12:51 +0530

    winner display

--- client/src/components/game/Endgame.js
+++ client/src/components/game/Endgame.js
@@ -1,6 +1,21 @@
+import { Image } from 'react-bootstrap';
+import gif1 from '../../images/trophy1.gif';
+import party from "party-js";
+import gif2 from '../../images/tropgy2.gif';
+
+
 const Endgame = ({ game, gameDispatch, socket }) => {
+
   return (
-    <div>winner: {game.winner.name}</div>
+    <div className="centerImage1">
+      <Image src={gif1} roundedCircle width="470" height="470" />
+      {/* <Image src={gif2} roundedCircle width="270" height="270" /> */}
+      
+      <div className="winner-Text">
+        {/* Winner : Disha Bundela */}
+        Winner : {game.winner.name}
+      </div>
+    </div>
   )
 }
 
