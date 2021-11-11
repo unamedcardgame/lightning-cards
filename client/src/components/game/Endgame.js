e25b78b5a255e0196de80ff5f1efe8e5b595f0d1
Eric Miranda <ericmiranda7@gmail.com>
2021-11-11 10:48:09 +0530

    remove unused

--- client/src/components/game/Endgame.js
+++ client/src/components/game/Endgame.js
@@ -1,16 +1,13 @@
 import { Image } from 'react-bootstrap';
 import gif1 from '../../images/trophy1.gif';
-import party from "party-js";
-import gif2 from '../../images/tropgy2.gif';
 
-
-const Endgame = ({ game, gameDispatch, socket }) => {
+const Endgame = ({ game }) => {
 
   return (
     <div className="centerImage1">
       <Image src={gif1} roundedCircle width="470" height="470" />
       {/* <Image src={gif2} roundedCircle width="270" height="270" /> */}
-      
+
       <div className="winner-Text">
         {/* Winner : Disha Bundela */}
         Winner : {game.winner.name}
